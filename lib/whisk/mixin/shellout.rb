6ef2f4be88095509d23a884fedea363b53a99386
Mathieu Sauve-Frankel <msf@kisoku.net>
2012-08-21 12:11:00 +0900

    add a very naive run_command, that is more silent than shell_out

--- lib/whisk/mixin/shellout.rb
+++ lib/whisk/mixin/shellout.rb
@@ -31,6 +31,12 @@ class Whisk
         cmd.run_command
         cmd
       end
+
+      def run_command(*command_args)
+        cmd = Mixlib::ShellOut.new(*command_args)
+        cmd.run_command
+        cmd
+      end
     end
   end
 end
