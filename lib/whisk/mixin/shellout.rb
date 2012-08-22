ec01713710ba36aaf2d5b46e9565df4c602dc5e9
Mathieu Sauve-Frankel <msf@qualcomm.com>
2012-08-22 19:09:56 +0900

    add shell_out!, also from chef/mixin/shellout

--- lib/whisk/mixin/shellout.rb
+++ lib/whisk/mixin/shellout.rb
@@ -32,6 +32,12 @@ class Whisk
         cmd
       end
 
+      def shell_out!(*command_args)
+        cmd = shell_out(*command_args)
+        cmd.error!
+        cmd
+      end
+
       def run_command(*command_args)
         cmd = Mixlib::ShellOut.new(*command_args)
         cmd.run_command
