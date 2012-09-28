0fa517f5809bc1bad03cc42a5255d7485bd693ca
Mathieu Sauve-Frankel <msf@qualcomm.com>
2012-09-28 21:15:51 +0900

    whitespace

--- examples/knife.rb
+++ examples/knife.rb
@@ -6,5 +6,5 @@ validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://localhost:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/msf/.chef/checksums' )
+cache_options(:path => '/home/msf/.chef/checksums')
 cookbook_path ENV['WHISK_COOKBOOK_PATH']
